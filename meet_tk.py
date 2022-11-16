3c92c63882289ed8512de456da0738553b5a7659
Saeed Gholami <saeed.ghollami@gmail.com>
2022-11-16 20:05:23 +0330

    Add buttons to make it intractive

--- meet_tk.py
+++ meet_tk.py
@@ -8,13 +8,25 @@ class MainWindow(tk.Tk):
         self.title("Hello World!")
 
         # Widgets of Main Window
-        label = ttk.Label(self, text="Hello World!")
-        label.pack(fill=tk.BOTH, expand=True, padx=30, pady=50)
+        self.label = ttk.Label(self, text="Choose one:")
+        self.label.pack(fill=tk.BOTH, expand=True, padx=30, pady=50)
+        hello_button = ttk.Button(self, text="Say Hello", command=self.say_hello)
+        hello_button.pack(side=tk.LEFT, padx=(20, 0), pady=(0, 20))
+        goodbye_button = ttk.Button(self, text="Say Goodbye", command=self.say_goodbye)
+        goodbye_button.pack(side=tk.RIGHT, padx=(0, 20), pady=(0, 20))
+
+    def say_hello(self):
+        self.label.configure(text="Hello World!")
+
+    def say_goodbye(self):
+        self.label.configure(text="Goodbye\nClosing after 2 seconds...")
+        self.after(2000, self.destroy)
 
 
 def main():
     app = MainWindow()
     app.mainloop()
+
 
 if __name__ == "__main__":
     main()
